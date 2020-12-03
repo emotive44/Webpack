d49b46662ecea2e565b274e62fa0b95a63246051
emotive44 <marko_streleshki_96@abv.bg>
2020-12-03 19:39:54 +0200

    fix bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import button from './components/button/button';
 import importImg from './importImg';
 import heading from './components/heading/heading';
+import './index.scss';
 
 importImg();
 button();
